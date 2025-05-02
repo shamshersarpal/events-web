e29f405bb346f22d8365f56066e59228ec817bde
shamshersarpal <shamshersingh01567@gmail.com>
2025-05-02 13:38:23 +0530

    update

---
+++ src/app/privacypolicy/page.jsx
@@ -0,0 +1,45 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Headerv2";
+
+export default function privacypolicy(){
+    return(
+        <>
+        <Header/>
+        <section className="w-full bg-[#ffecdb] lg:pt-20 pt-10 pb-0 ">
+                <h2 className="font-[700] lg:text-4xl text-2xl text-gray-900 text-center pt-10"> Privacy Policy</h2>
+                <div className="w-full bg-white lg:py-20 py-5 lg:mt-20 mt-10 lg:rounded-t-[55px] rounded-t-[15px] lg:px-0 px-4">
+                    <div className="max-w-screen-xl m-auto">
+                        
+                            <div className="mb-4">
+                                <h3 className="text-2xl mb-2 font-[600]">1. Introduction</h3>
+                                <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+
+                            <div className="mb-4">
+                                <h3 className="text-2xl mb-2 font-[600]">2. Information We Collect
+                                </h3>
+                                <p className="text-gray-600 mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                                <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+
+                            <div className="mb-4">
+                                <h3 className="text-2xl mb-2 font-[600]">3. How We Use Your Information
+                                </h3>
+                                <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+
+                            <div className="mb-4">
+                                <h3 className="text-2xl mb-2 font-[600]">4. Sharing of Information
+                                </h3>
+                                <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+
+                        </div>
+                        </div>
+                        </section>
+
+        <Footer/>
+        </>
+    )
+}
